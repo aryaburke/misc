bc8aec633797f54e998cbbb192fee0b859a80a3e
arya <arya@chariotmove.com>
2023-02-24 14:02:02 -0500

    working set up env

---
+++ lowercontacts/README.txt
@@ -0,0 +1,4 @@
+lowercases the names of contacts for the ~*aesthetic*~
+
+1. `pip install requirements.txt`
+2. `python3 lowercontacts.py {input_file_path}.vcf {output_file_path}.vcf`
--- lowercontacts/lowercontacts.py
+++ lowercontacts/lowercontacts.py
@@ -1,9 +1,12 @@
 import vcf
 import sys
 
-# to be run in the format `lowercontacts.py {filename}.vcf`
+# to be run in the format `python3 lowercontacts.py {filename}.vcf`
 
 contacts = sys.argv[1]
 
 reader = vcf.Reader(filename=contacts)
 writer = vcf.Writer(open('./loweredcontacts.vcf', 'w'), reader)
+
+for contact in reader:
+    print(contact)
